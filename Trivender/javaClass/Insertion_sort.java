e8ba3cf2ca06de6c58547fb04c1d22cbba2109c7
Trivender <choudharyrobin2220@gmail.com>
2022-08-03 00:22:52 +0530

    sorting

--- Trivender/javaClass/Insertion_sort.java
+++ Trivender/javaClass/Insertion_sort.java
@@ -1,10 +1,37 @@
 package javaClass;
 
+import java.util.Scanner;
+
 public class Insertion_sort {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int[]arr=new int[n];
+		//int[] arr = { 4, 5, 2, 7, 8 };
+		for(int i=0;i<arr.length;i++) {
+			arr[i]=sc.nextInt();
+		}
+		Sort(arr);
+		for (int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i] + " ");
+		}
+	}
 
+	public static void Sort(int[] arr) {
+
+		for (int i = 1; i < arr.length; i++) {
+			int j = i - 1;
+			int temp = arr[i];
+			while (j >= 0 && arr[j] > temp) {
+				arr[j + 1] = arr[j];
+				j--;
+			}
+			j++;
+			arr[j] = temp;
+
+		}
 	}
 
 }
