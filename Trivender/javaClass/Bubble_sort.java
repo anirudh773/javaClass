469eac6586f439c4931893b80e6c496b67328453
Trivender <choudharyrobin2220@gmail.com>
2022-08-02 21:38:43 +0530

    Sorting

---
+++ Trivender/javaClass/Bubble_sort.java
@@ -0,0 +1,29 @@
+package javaClass;
+
+public class Bubble_sort {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] arr = { 5, 4, 3, 2, 1 };
+		Sort(arr);
+		for (int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i]+" ");
+		}
+
+	}
+
+	public static void Sort(int[] arr) {
+
+		for (int i = 1; i < arr.length; i++) {
+			for (int j = 0; j < arr.length - i; j++) {
+				if (arr[j] > arr[j + 1]) {
+					int t = arr[j];
+					arr[j] = arr[j + 1];
+					arr[j + 1] = t;
+				}
+
+			}
+		}
+	}
+
+}
---
+++ Trivender/javaClass/Insertion_sort.java
@@ -0,0 +1,10 @@
+package javaClass;
+
+public class Insertion_sort {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
